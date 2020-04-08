8807d68513cf17150f33e2cb9d50259067f57127
winsw <winsw@DESKTOP-I0670TL>
2020-04-08 09:15:32 +0800

    1.第二次测试。

---
+++ src/main/java/com/cup/testDouble/TestDouble.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cup.testDouble;
+
+/**
+ * 用于测试 double 和 Double的计算速度
+ *
+ * @author winsway
+ */
+public class TestDouble {
+
+    /**
+     * 测试double 和 Double的计算速度
+     */
+    public void testSpeed() {
+        double doubleNum = 0;
+        Double A = 0.0;
+        int loopCount = 1000000;
+        //计算double类型数据的循环耗时
+        long timer1 = System.currentTimeMillis();
+        for (int i = 0; i < loopCount; i++) {
+            doubleNum += 1.80;
+        }
+        long timer2 = System.currentTimeMillis();
+        System.out.printf("循环%dW次的合计值: %f\n", loopCount / 10000, doubleNum);
+        System.out.println("总共耗时为: " + (timer2 - timer1) + "毫秒");
+        //计算Double类型数据的循环耗时
+        long timer3 = System.currentTimeMillis();
+        for (int i = 0; i < loopCount; i++) {
+            A += 1.80;
+        }
+        long timer4 = System.currentTimeMillis();
+        System.out.printf("循环%dW次的合计值: %f\n", loopCount / 10000, A);
+        System.out.println("总共耗时为: " + (timer4 - timer3) + "毫秒");
+    }
+
+}
---
+++ src/test/java/com/cup/testDouble/TestDoubleTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cup.testDouble;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author winsw
+ */
+public class TestDoubleTest {
+
+    public TestDoubleTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of testSpeed method, of class TestDouble.
+     */
+    @Test
+    public void testTestSpeed() {
+        System.out.println("testSpeed");
+        TestDouble instance = new TestDouble();
+        instance.testSpeed();
+    }
+
+}
